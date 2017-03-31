88b870ab69cd77c4c1fd302b64a81f3fb540d992
wwd <wushang1987@gmail.com>
2017-04-01 00:24:48 +0800

    新增一些常用工具

---
+++ jsdoc/test.js
@@ -0,0 +1,10 @@
+/**
+ * print hello name
+ * @param {*string} name 
+ */
+function hello(name) {
+    console.log('hello' + name);
+
+}
+
+hello("wwd");
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
 var path = require('path');
+var JsDocPlugin = require('jsdoc-webpack-plugin');
 console.log(__dirname);
 
 module.exports = {
-  entry: './views/app.js',
-  output: {
-    filename: 'bundle.js',
-    // path: path.resolve(__dirname+'\\views\\', 'dist')
-    path: path.resolve(__dirname+'\\views\\dist')
-    // path: ""
-  // }
-  }
+    entry: './views/app.js',
+    output: {
+        filename: 'bundle.js',
+        // path: path.resolve(__dirname+'\\views\\', 'dist')
+        path: path.resolve(__dirname + '\\views\\dist')
+            // path: ""
+            // }
+    },
+    plugins: [
+        new JsDocPlugin({
+            conf: './jsdoc.conf'
+        })
+    ]
 };
-
